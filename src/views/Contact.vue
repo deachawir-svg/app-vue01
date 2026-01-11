5bbf7c9ca1581e399a1d4a3ddf85701eed2b9631
deachawir-svg <deacha.wir@gmail.com>
2026-01-11 18:36:51 +0700

    update v2

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,49 @@
 <template lang="">
-    <div>
+    <div class="container">
         <h1>Contact to Deacha Wiriyagul</h1>
         <h3>Tel.092-223-3634</h3>
         <img alt="Vue logo" src="../assets/Office.jpg">
+        <br><br><br>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img alt="Vue logo" width="90%" src="../assets/MPC.jpg">
+    </div>
+
+    <div class="col-sm-6 bg-body-tertiary">
+       <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Detail</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+    <div class="mb-3  text-center">
+        <button type="button" class="btn btn-primary">Submit</button>&nbsp;
+        <button type="button" class="btn btn-secondary">Cancel</button>
+    </div>
+
+
+    </div>
+    
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+   
+
     </div>
 </template>
 
