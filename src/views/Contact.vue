1d037aa5fb9704abb1567ba8d9607ea9fc2112a7
deachawir-svg <deacha.wir@gmail.com>
2026-01-11 16:42:34 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template lang="">
+    <div>
+        <h1>Contact to Deacha Wiriyagul</h1>
+        <h3>Tel.092-223-3634</h3>
+        <img alt="Vue logo" src="../assets/Office.jpg">
+    </div>
+</template>
+
